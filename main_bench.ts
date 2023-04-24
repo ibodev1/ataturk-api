eb1ad222657479d6c049cb44d668af522e02b7be
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2023-04-24 16:25:31 +0300

    update: main files and deno.json

--- main_bench.ts
+++ main_bench.ts
@@ -1,9 +1,3 @@
-import { add } from "./main.ts";
-
-Deno.bench(function addSmall() {
-  add(1, 2);
-});
-
-Deno.bench(function addBig() {
-  add(2 ** 32, 2 ** 32);
+Deno.bench(function bench() {
+  //! bench
 });
